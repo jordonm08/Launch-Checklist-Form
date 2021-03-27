0514d900801a25c2abf4da4ccd3246d7da1b7efb
Admin <admin@Admins-MacBook-Air.local>
2021-03-27 11:40:01 -0500

    updated the if/else statements and conditionally present mission

--- script.js
+++ script.js
@@ -17,40 +17,43 @@ window.addEventListener("load", function() {
          document.getElementById("fuelStatus").innerHTML = `yeah... ${fuelLevelInput}L of fuel is not gonna get us there champ`
          document.getElementById("launchStatus").style.color = 'red'
          document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch`
+      } else {
+         document.getElementById("fuelStatus").innerHTML = `plenty of fuel`
       }
       if(cargoMassInput > 10000) {
          document.getElementById("faultyItems").style.visibility = "visible";
          document.getElementById("cargoStatus").innerHTML = `yeah... ${cargoMassInput}kg is a bit too much weight for this ship`
          document.getElementById("launchStatus").style.color = 'red'
          document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch`
+      } else {
+         document.getElementById("cargoStatus").innerHTML = `the cargo is light enough`
       }
       
       if(fuelLevelInput >= 10000 && cargoMassInput <= 10000 ){
          document.getElementById("faultyItems").style.visibility = "hidden"
          document.getElementById("launchStatus").innerHTML = `Shuttle ready for launch... finally`
          document.getElementById("launchStatus").style.color = 'green'
-      }
-
-      let api = "https://handlers.education.launchcode.org/static/planets.json"
+         
+         let api = "https://handlers.education.launchcode.org/static/planets.json"
       
-      fetch(api).then(function(response) {
-         response.json().then(function(json){
-            let i = json[Math.floor(Math.random() * json.length)];
-            console.log(i)
-            document.getElementById("missionTarget").innerHTML = `
-               <h2>Mission Destination</h2>
-                <ul>
-                   <li>Name: ${i.name}</li>
-                   <li>Diameter: ${i.diameter}</li>
-                   <li>Star: ${i.star}</li>
-                   <li>Distance from Earth: ${i.distance}</li>
-                   <li>Number of Moons: ${i.moons}</li>
-               </ul>
-               <img src="${i.image}">
-            `
-         })
-      })
-
+         fetch(api).then(function(response) {
+            response.json().then(function(json){
+               let i = json[Math.floor(Math.random() * json.length)];
+               console.log(i)
+               document.getElementById("missionTarget").innerHTML = `
+                  <h2>Mission Destination</h2>
+                  <ul>
+                     <li>Name: ${i.name}</li>
+                     <li>Diameter: ${i.diameter}</li>
+                     <li>Star: ${i.star}</li>
+                     <li>Distance from Earth: ${i.distance}</li>
+                     <li>Number of Moons: ${i.moons}</li>
+                  </ul>
+                  <img src="${i.image}">
+               `
+            });
+         });
+      };
    });
 });
 
